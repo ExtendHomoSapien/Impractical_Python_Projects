1263d653102e7cd2bc7958a522575ca5f29121ed
James <jkcarrolliv@gmail.com>
2023-01-01 12:51:40 -0500

    pg34: recursive palindrome exercise. cleaning up pylint evaluation. (JC)

--- Chapter_2/recursive_palindrome.py
+++ Chapter_2/recursive_palindrome.py
@@ -1,11 +1,16 @@
-"""So here's how we can recursively determine whether a string is a palindrome. If the first and last letters differ,
+"""Recursive palindrome pseudocode.
+
+So here's how we can recursively determine whether a string is a palindrome. If the first and last letters differ,
 then declare that the string is not a palindrome. Otherwise, strip off the first and last letters, and determine
 whether the string that remains—the subproblem—is a palindrome. Declare the answer for the shorter string to be the
 answer for the original string. Once you get down to a string with no letters or just one letter, declare it to be a
-palindrome. """
+palindrome.
+https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/using-recursion-to-determine-whether-a-word-is-a-palindrome
+"""
 
 
 def is_palindrome(word: str) -> bool:
+    """Evaluate word for palindrome."""
     if word == '' or len(word) == 1:
         return True
     if word[0:1] == word[-1:]:
@@ -17,15 +22,14 @@ def is_palindrome(word: str) -> bool:
 
 if __name__ == "__main__":
     eval_words = [
-        'ewe',
-        'ewok', 'broken', 'typical', 'evaporate', 'racecar'
+        'ewe', 'ewok', 'broken', 'typical', 'evaporate', 'racecar'
     ]
-    p_count = 0
+    palindrome_count = int()  # pylint: disable=C0103
     for w in eval_words:
         print(f'Evaluating "{w}" for palindrome')
         if is_palindrome(w):
             print(f'"{w}" is palindrome')
-            p_count += 1
+            palindrome_count += 1
 
-    print(f'Total palindromes in list: {p_count}')
+    print(f'Total palindromes in list: {palindrome_count}')
     print(f'Total words in list: {len(eval_words)}')
