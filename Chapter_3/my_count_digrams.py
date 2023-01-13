eb9673f476eccf78d4d408c58d3dbf2719a3b9bb
James <jkcarrolliv@gmail.com>
2023-01-12 20:48:33 -0500

    CH3: Digram statistics exercise final commit (JC)

--- Chapter_3/my_count_digrams.py
+++ Chapter_3/my_count_digrams.py
@@ -1,6 +1,10 @@
-"""Write a python program that finds all the digrams in 'tmvoordle' and then counts their frequency of occurrence in
-a dictionary file. Be sure to test your code on words like volvo, so you don't overlook repeating digrams in the same
-word. """
+"""
+Capture bigram stats on a dictionary.
+
+Write a python program that finds all the bigrams in 'tmvoordle' and then counts their frequency of occurrence in
+a dictionary file. Be sure to test your code on words like volvo, so you don't overlook repeating bigrams in the same
+word.
+"""
 
 import word_dictionary_util.load_dictionary as load_dictionary
 from collections import Counter
@@ -11,27 +15,38 @@ from collections import Counter
 # loop dictionary and find bigrams, if match, add to count
 
 def make_bigrams(word: str) -> list:
-    """split word into bigrams"""
+    """Split word into bigrams."""
     out_bigrams = list()
     for i in range(0, len(word) - 1):
         bigram = word[i] + word[i + 1]
         out_bigrams.append(bigram)
     return out_bigrams
 
+
+def intersection(candidate: list, control: list):
+    """Filter candidate list by control list."""
+    temp = set(control)
+    filtered = [value for value in candidate if value in temp]
+    return filtered
+
+
 def filter_bigrams(word: str, target_bigrams: list) -> list:
-    """return list of bigrams matching target bigrams"""
-    pass
+    """Return list of bigrams matching target bigrams."""
+    return intersection(candidate=make_bigrams(word), control=target_bigrams)
+
 
 def main():
+    """Control logic."""
     dict_file = load_dictionary.load()
     input_word = 'tmvoordle'
-    bigram_dict = Counter(make_bigrams(input_word))
+    input_bigrams = set(make_bigrams(input_word))
     filtered_bigrams = []
-
-    # for bigram in make_bigrams(input_word):
-    #     print(f"finding bigram counts for '{bigram}'")
-    #     for word in dict_file:
-    #         word
+    for word in dict_file:
+        filtered_bigrams.extend(
+            filter_bigrams(word=word, target_bigrams=list(input_bigrams))
+        )
+    bg_cntr = Counter(filtered_bigrams)
+    print(bg_cntr.most_common())
 
 
 if __name__ == "__main__":
--- Chapter_3/test_my_count_digrams.py
+++ Chapter_3/test_my_count_digrams.py
@@ -25,3 +25,13 @@ class TestFilterBigrams(TestCase):
             my_count_digrams.filter_bigrams('volvo', ['vo']),
             ['vo', 'vo']
         )
+
+class TestIntersection(TestCase):
+    def test_filter_bigrams(self):
+        lst1 = [9, 9, 74, 21, 45, 11, 63]
+        lst2 = [4, 9, 1, 17, 11, 26, 28, 28, 26, 66, 91]
+
+        self.assertEqual(
+            my_count_digrams.intersection(candidate=lst1, control=lst2),
+            [9, 9, 11]
+        )
